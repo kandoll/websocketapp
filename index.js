36d73fcfb1da7679ce6268fa5e922dc93b822ca7
Kanika <kan_dawalia@yahoo.in>
2019-07-15 18:49:18 +0530

    created server and estabished connection

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express= require('express');
-
+var socket=require('socket.io');
 
 //App setup
 var app=express();
@@ -12,3 +12,16 @@ var server= app.listen(4000,()=>{
 
 //static files
 app.use(express.static('public'))
+
+//socket setup
+
+var io=socket(server);  
+
+/*socket is a function
+ which takes in a parameter -the server w hich
+  we wont to work with,which in this case is server that we created */
+
+io.on('connection',(socket)=>{
+    console.log('socket connection established')
+})
+//if 10 different clients want to make a connection each one will have a different socket
